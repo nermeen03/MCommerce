c8030ba859090f4c03ad5f12dc6c3907722b67ee
jailanmedhat <jailanmedhatt2@gmail.com>
2025-06-17 02:17:06 +0300

    network

--- MCommerce/Data/DataSources/RemoteSource/RemoteSource.swift
+++ MCommerce/Data/DataSources/RemoteSource/RemoteSource.swift
@@ -6,9 +6,45 @@
 //
 
 import Foundation
-
+import Alamofire
 class ApiCalling {
     let networkService = NetworkService.shared
+    func createCustomer() {
+        guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] ,let apiKey = Bundle.main.infoDictionary?["API_KEY"], let token = Bundle.main.infoDictionary?["ADMIN_TOKEN"],let key = Bundle.main.infoDictionary?["ADMIN_KEY"] else{
+            return
+        }
+        
+        let url = "https://\(apiKey):\(token)\(key)@\(baseURL)/admin/api/2022-01/customers.json"
+          
+            let headers: HTTPHeaders = [
+                "Content-Type": "application/json"
+            ]
+
+            let parameters: [String: Any] = [
+                "customer": [
+                    "first_name": "Nermeen",
+                    "id" : "1024064673",
+                    "last_name": "Mohamed",
+                    "email": "nermeennnn55@gmail.com",
+                    "phone": "+201234567840",
+                    "verified_email": true,
+                    "password": "YourSecurePassword12@",
+                    "password_confirmation": "YourSecurePassword12@",
+                    "accepts_marketing": true
+                ]
+            ]
+
+            AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+                .validate()
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let value):
+                        print("✅ Customer created: \(value)")
+                    case .failure(let error):
+                        print("❌ Error: \(error)")
+                    }
+                }
+        }
     
     func callRestApi() {
         
@@ -17,6 +53,7 @@ class ApiCalling {
         }
         
         let url = "https://\(apiKey):\(token)\(key)@\(baseURL)/admin/api/2022-01/collects.json"
+        print(url)
 
         networkService.request(url: url, method: .GET , responseType: Test.self, completion: {
             result in

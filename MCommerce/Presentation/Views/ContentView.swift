7825d2f037214a6e17ce2e7c69d2e45225ddc484
jailanmedhat <jailanmedhatt2@gmail.com>
2025-06-17 20:16:05 +0300

    UserDefaults adde

--- MCommerce/Presentation/Views/ContentView.swift
+++ MCommerce/Presentation/Views/ContentView.swift
@@ -9,25 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Button("Test", action: {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//            Button("Test", action: {
 
               //  ApiCalling().callRestApi()
                // ApiCalling().createCustomer()
 
                 //createCustomer()
-                getCustomer()
+           //     getCustomer()
 
-            })
-        }
-        .padding()
+//            })
+//        }
+//        .padding()
        
       //  HomeView()
 //        MainTabView()
+    //Register()
+        if UserDefaultsManager.shared.isLoggedIn() {
+                     HomeView()
+                 } else {
+                     WelcomeScreen()
+                 }
     }
 }
 
@@ -35,62 +41,6 @@ struct ContentView: View {
     ContentView()
 }
 
-func createCustomer() {
-    let createCustomerMutation = """
-    mutation customerCreate($input: CustomerCreateInput!) {
-      customerCreate(input: $input) {
-        customer {
-          id
-          email
-          firstName
-          lastName
-        }
-        customerUserErrors {
-          field
-          message
-        }
-      }
-    }
-    """
-
-    let variables: [String: Any] = [
-        "input": [
-            "firstName": "Nermeen",
-            "lastName": "Mohamed",
-            "email": "flued1949@teleworm.us",
-            "phone": "+201232864499",
-            "password": "YourSecurePassword",
-            "acceptsMarketing": true
-        ]
-    ]
-
-    ApiCalling().callQueryApi(query: createCustomerMutation, variables: variables)
-}
 
 
-func getCustomer(){
-    let loginQuery = """
-    mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
-      customerAccessTokenCreate(input: $input) {
-        customerAccessToken {
-          accessToken
-          expiresAt
-        }
-        userErrors {
-          field
-          message
-        }
-      }
-    }
-    """
-
-    let variables: [String: Any] = [
-        "input": [
-            "email": "flued1949@teleworm.us",
-            "password": "YourSecurePassword"
-        ]
-    ]
-
-    ApiCalling().callQueryApi(query: loginQuery, variables: variables)
-}
 

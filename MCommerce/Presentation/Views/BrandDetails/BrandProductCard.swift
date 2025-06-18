bcb0fccfebb79a0ca05535fc19a9d0bae70e891f
Abram <abram.morris62@gmail.com>
2025-06-18 03:11:13 +0300

    Getting Brand Data and displaying it and Creating BrandScreen UI

---
+++ MCommerce/Presentation/Views/BrandDetails/BrandProductCard.swift
@@ -0,0 +1,18 @@
+//
+//  BrandProductCard.swift
+//  MCommerce
+//
+//  Created by abram on 18/06/2025.
+//
+
+import SwiftUI
+
+struct BrandProductCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrandProductCard()
+}
---
+++ MCommerce/Presentation/Views/Tab Bar/MainTabView.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabView.swift
+//  MCommerce
+//
+//  Created by abram on 16/06/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab: Tab = .home
+    @State private var cartBadgeCount: Int = 3 
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Group {
+                switch selectedTab {
+                case .home:
+                    HomeView()
+                case .search:
+                    SearchView()
+                case .cart:
+                    CartView()
+                case .profile:
+                    ProfileView()
+                }
+            }
+            FloatingTabBar(selectedTab: $selectedTab, cartBadgeCount: cartBadgeCount)
+                
+        }
+    }
+}
+
+
+#Preview {
+    MainTabView()
+}

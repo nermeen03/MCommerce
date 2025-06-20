bd68aa6e12b7990ab11a07d5422245e4d12daac9
jailanmedhat <jailanmedhatt2@gmail.com>
2025-06-20 03:48:49 +0300

    some changes

--- MCommerce/Data/DataSources/RemoteSource/FireBaseAuth.swift
+++ MCommerce/Data/DataSources/RemoteSource/FireBaseAuth.swift
@@ -9,26 +9,29 @@ import FirebaseAuth
 class FireBaseAuthHelper{
   static  let shared =  FireBaseAuthHelper()
     private init() {}
+
+
     func registerWithFirebase(email: String, password: String) {
-        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-            if let user = authResult?.user {
-                if !user.isEmailVerified {
-                    user.sendEmailVerification { error in
-                        if let error = error {
-                            print("Error sending verification email: \(error.localizedDescription)")
-                        } else {
-                            print("Verification email sent to \(user.email ?? "")")
-                        }
-                    }
-                } else {
-                    print("Email already verified.")
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                guard let user = authResult?.user
+                else {
+                    let error = error
+                    print(error?.localizedDescription ?? "")
+                   
+                    
+                    return
+//                    print("Registration failed: \(error.localizedDescription)")
+                    
                 }
-            } else if let error = error {
-                print("Sign in failed: \(error.localizedDescription)")
+                user.sendEmailVerification { error in
+                    if let error = error {
+                        print("Error sending verification email: \(error.localizedDescription)")
+                    } else {
+                        print("Verification email sent to \(user.email ?? "")")
+                    }
+                }
             }
         }
-    }
-   
 
     func checkEmailVerificationStatus(completion: @escaping (Bool) -> Void) {
         guard let user = Auth.auth().currentUser else {

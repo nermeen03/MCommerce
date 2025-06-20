abb57e59216b3e5be9f0427e9cb0f6eaf6fbb32c
Nermeen Mohamed <nermeen41292@gmail.com>
2025-06-21 02:02:45 +0300

    Clean Profile & Settings

--- MCommerce/Data/Repos/RemoteRepo/OrderRepo.swift
+++ MCommerce/Data/Repos/RemoteRepo/OrderRepo.swift
@@ -7,39 +7,44 @@
 
 import Foundation
 
-struct OrderRepo {
-    func getOrderTest(completion : @escaping (Result<OrdersResponse, NetworkError>) -> Void){
+struct OrderRepo {    
+    func getOrderTest(completion: @escaping (Result<[OrderDataResponse], NetworkError>) -> Void) {
+        let customerId = UserDefaultsManager.shared.getUserId() ?? "customerId"
         let query = """
-            {
-              orders(first: 5) {
-                edges {
-                  node {
-                    id
-                    name
-                    createdAt
-                    totalPriceSet {
-                      shopMoney {
-                        amount
-                        currencyCode
-                      }
+        {
+          customer(id: "\(customerId)") {
+            id
+            firstName
+            lastName
+            email
+            orders(first: 5) {
+              edges {
+                node {
+                  id
+                  name
+                  createdAt
+                  totalPriceSet {
+                    shopMoney {
+                      amount
+                      currencyCode
                     }
-                    lineItems(first: 5) {
-                      edges {
-                        node {
-                          title
-                          quantity
-                          variant {
-                            id
-                            image {
-                              originalSrc
-                            }
-                            product {
-                              images(first: 1) {
-                                edges {
-                                  node {
-                                    originalSrc
-                                    altText
-                                  }
+                  }
+                  lineItems(first: 5) {
+                    edges {
+                      node {
+                        title
+                        quantity
+                        variant {
+                          id
+                          image {
+                            originalSrc
+                          }
+                          product {
+                            images(first: 1) {
+                              edges {
+                                node {
+                                  originalSrc
+                                  altText
                                 }
                               }
                             }
@@ -51,68 +56,18 @@ struct OrderRepo {
                 }
               }
             }
-
-            """
-        ApiCalling().callQueryApi(query: query,useToken: true ,completion: {(result : Result<OrdersResponse, NetworkError>) in
-            completion(result)
-        })
+          }
+        }
+        """
+        
+        ApiCalling().callQueryApi(query: query, useToken: true) { (result: Result<OrdersResponse, NetworkError>) in
+            switch result{
+            case .success(let response):
+                completion(.success(Mapper.mapToOrderData(from: response)))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
-    
-//    func getOrderTest(completion: @escaping (Result<OrdersResponse, NetworkError>) -> Void) {
-//        let customerId = UserDefaultsManager.shared.getUserId() ?? "customerId"
-//        let query = """
-//        {
-//          customer(id: "\(customerId)") {
-//            id
-//            firstName
-//            lastName
-//            email
-//            orders(first: 5) {
-//              edges {
-//                node {
-//                  id
-//                  name
-//                  createdAt
-//                  totalPriceSet {
-//                    shopMoney {
-//                      amount
-//                      currencyCode
-//                    }
-//                  }
-//                  lineItems(first: 5) {
-//                    edges {
-//                      node {
-//                        title
-//                        quantity
-//                        variant {
-//                          id
-//                          image {
-//                            originalSrc
-//                          }
-//                          product {
-//                            images(first: 1) {
-//                              edges {
-//                                node {
-//                                  originalSrc
-//                                  altText
-//                                }
-//                              }
-//                            }
-//                          }
-//                        }
-//                      }
-//                    }
-//                  }
-//                }
-//              }
-//            }
-//          }
-//        }
-//        """
-//        
-//        ApiCalling().callQueryApi(query: query, useToken: true) { (result: Result<OrdersResponse, NetworkError>) in
-//            completion(result)
-//        }
-//    }
 
 }
---
+++ MCommerce/Domain/UseCases/CartUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  CartUseCase.swift
+//  MCommerce
+//
+//  Created by Nermeen Mohamed on 20/06/2025.
+//
+
+class AddInCartUseCase{
+    
+}
+
+class RemoveFromCartUseCase{
+    
+}
+
+class GetCartUseCase{
+    
+}

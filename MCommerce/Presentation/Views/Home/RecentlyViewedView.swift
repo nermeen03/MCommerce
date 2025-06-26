ad5c3986860a379a1ad633a87811011f1c18b7f4
Abram <abram.morris62@gmail.com>
2025-06-26 16:23:35 +0300

    modifing ui

--- MCommerce/Presentation/Views/Home/RecentlyViewedView.swift
+++ MCommerce/Presentation/Views/Home/RecentlyViewedView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct RecentlyViewedView: View {
-    @Binding var products : [Product]
+    @Binding var products: [Product]
     @EnvironmentObject var coordinator: AppCoordinator
-    
+
     var body: some View {
         VStack(alignment: .leading) {
             Text("You Might Like")
@@ -18,16 +18,24 @@ struct RecentlyViewedView: View {
                 .bold()
 
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 15) {
                     ForEach(products) { product in
-                        ProductCard(title:  product.title  , imageUrl: product.imageUrl).onTapGesture {
+                        ProductHomeCard(
+                            imageUrl: product.imageUrl ?? "",
+                            title: extractTextBetweenPipes(from: product.title),
+                            price: "$\(product.price)",
+//                            backgroundColor: Color.white
+                        )
+                        .onTapGesture {
                             coordinator.navigate(to: .productInfo(product: product.id))
                         }
                     }
                 }
+                .padding(.horizontal)
             }
         }
     }
+
     func extractTextBetweenPipes(from title: String) -> String {
         let parts = title.split(separator: "|", omittingEmptySubsequences: false)
         if parts.count >= 3 {
@@ -38,3 +46,34 @@ struct RecentlyViewedView: View {
     }
 }
 
+//struct RecentlyViewedView: View {
+//    @Binding var products : [Product]
+//    @EnvironmentObject var coordinator: AppCoordinator
+//    
+//    var body: some View {
+//        VStack(alignment: .leading) {
+//            Text("You Might Like")
+//                .font(.title3)
+//                .bold()
+//
+//            ScrollView(.horizontal, showsIndicators: false) {
+//                HStack {
+//                    ForEach(products) { product in
+//                        ProductCard(title:  product.title  , imageUrl: product.imageUrl).onTapGesture {
+//                            coordinator.navigate(to: .productInfo(product: product.id))
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    func extractTextBetweenPipes(from title: String) -> String {
+//        let parts = title.split(separator: "|", omittingEmptySubsequences: false)
+//        if parts.count >= 3 {
+//            return parts[1].trimmingCharacters(in: .whitespaces)
+//        } else {
+//            return title
+//        }
+//    }
+//}
+//
